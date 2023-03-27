61cf06a726061a4711262ed9e00b258ecd9b7f11
problem-sudo <anitmaurya563@gmail.com>
2023-03-27 22:05:54 +0530

    First commit remove warning

---
+++ src/apiKeys.js
@@ -0,0 +1,5 @@
+// Visit https://api.openweathermap.org & then signup to get our API keys for free
+module.exports = {
+  key: "14f9c2c47a64ae1dc661386eca683931",
+  base: "https://api.openweathermap.org/data/2.5/",
+};
