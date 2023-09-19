6ac7be6f37c103b47553b55a543798429747c1f8
problem-sudo <anitmaurya563@gmail.com>
2023-09-20 00:41:45 +0530

    First commit

---
+++ src/apiKeys.js
@@ -0,0 +1,5 @@
+// Visit https://api.openweathermap.org & then signup to get our API keys for free
+module.exports = {
+  key: "7dd87093e1683c9669f44286306d3d28",
+  base: "https://api.openweathermap.org/data/2.5/",
+};
